cd85ff27d8d24796a64cc9239c8930c1d2438e1b
sipan younan <sipannyounan@gmail.com>
2026-02-23 12:24:33 +0330

    fix sticky header styles

--- src/base/logo/Logo.tsx
+++ src/base/logo/Logo.tsx
@@ -1,10 +1,13 @@
 function Logo() {
   return (
-    <div className="flex items-center justify-start gap-4 select-none text-sm">
+    <div
+      dir="ltr"
+      className="flex items-center justify-start gap-2 select-none text-sm "
+    >
       <div className="border-2 border-wood-dark px-3 py-1 rounded-md text-wood font-bold sm:tracking-widest ">
         WOOD
       </div>
-      <span className="text-wood font-bold sm:text-3xl sm:tracking-widest">
+      <span className="text-wood font-bold sm:text-4xl sm:tracking-widest">
         CRAFT
       </span>
     </div>
